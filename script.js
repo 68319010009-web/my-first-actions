f8ac13b6f528b34c1c370bb3c3c4a7de4de55f14
68319010009-web <68319010009-web@users.noreply.github.com>
2026-06-29 06:53:26 +0000

    deploy: 1f823350d8723c911f02b6d58321b82911e35263

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-document.getElementById("msg").innerText = "ระบบ CI/CD ทำงานได้อย่างถูกต้อง!"; 
+const msg = document.getElementById("msg");
+
+function showMessage(){
+
+    msg.innerHTML = `
+    ✅ ระบบ CI/CD ทำงานได้อย่างถูกต้อง! <br><br>
+    🎉 Deploy สำเร็จผ่าน GitHub Actions <br>
+    🌐 เว็บไซต์ออนไลน์ด้วย GitHub Pages
+    `;
+
+    msg.style.color = "green";
+}
+
+// แสดงข้อความอัตโนมัติเมื่อเปิดเว็บ
+window.onload = function(){
+
+    setTimeout(() => {
+
+        msg.innerHTML = "🚀 Welcome to my GitHub Pages Website";
+        msg.style.color = "#0077ff";
+
+    },500);
+
+}
